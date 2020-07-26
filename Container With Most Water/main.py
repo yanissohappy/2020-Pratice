9123792e7cce23062efa1a4c9eb63f2b87a51949
Yan <41467560+yanissohappy@users.noreply.github.com>
2020-07-26 14:25:34 +0800

    Revise program file

--- Container With Most Water/main.py
+++ Container With Most Water/main.py
@@ -1,31 +1,36 @@
-def isValid(s):
-    stack = []
-    i = 0
-    if not s:
-        return True
-    if len(s) == 1:
-        return False
-    while i < len(s):
-        if s[i] == '(' or s[i] == '{' or s[i] == '[':
-            stack.append(s[i])
-        else:
-            stack.append(s[i])
-            if stack:
-                if len(stack) >= 2:
-                    right_pop_char = stack.pop()
-                    left_pop_char = stack.pop()
-                    if left_pop_char == '(' and right_pop_char != ')':
-                        return False
-                    elif left_pop_char == '{' and right_pop_char != '}':
-                        return False
-                    elif left_pop_char == '[' and right_pop_char != ']':
-                        return False
-                else:
-                    return False
-        i += 1
-    if not stack:
-        return True
-    else:
-        return False
+def maxArea(self, height):
+    """ 註解的這邊是可以的，只是會變成O(n^2)，效能很差
+    max_val = 0
+    val = 0
+    
+    total_len = len(height) - 1
+    # 先從最大的寬度開始找
+    
+    while True: # 做到total_len為止
+        if total_len == 0:
+            break 
+        From = 0               
+        while From + total_len < len(height):
+            To = From + total_len 
+            if height[From] >= height[To]:
+                val = height[To] * total_len
+            else:
+                val = height[From] * total_len
+            if val >= max_val:
+                max_val = val
+            From += 1
+        total_len -= 1
+        
+    return max_val
+    """            
 
-print(isValid("(([]){})"))
+    max_val, left, right = 0, 0, len(height) - 1
+    
+    while left < right:
+        max_val = max(max_val, min(height[right], height[left]) * (right - left))
+        
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+    return max_val
