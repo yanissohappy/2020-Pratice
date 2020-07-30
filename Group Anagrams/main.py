14969bca4a3ea09aa6afcccaf5f50c2f20c27926
Yan <41467560+yanissohappy@users.noreply.github.com>
2020-07-30 21:12:57 +0800

    Group Anagrams

---
+++ Group Anagrams/main.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        # check len, and every char in a word
+        # 方法1: 直接把每個字排列組合然後比對，但如果一個字超長那就絕對不是好方法
+        # 方法2: 所以我覺得直接比長度和有沒有每個字的個數都一樣就好
+        # 做一個字典list
+        
+        list_dic = []
+        
+        for i in range(len(strs)):
+            dic = {}
+            for j in range(len(strs[i])):
+                if strs[i][j] not in dic:
+                    dic[ strs[i][j] ] = 1
+                else:
+                    dic[ strs[i][j] ] += 1                            
+            list_dic.append(dic)
+        
+        rev = []
+        list_num = 0
+        
+        for i in range(len(list_dic)):
+            j = 0
+            temp = []
+            if list_dic:
+                compared_dic = list_dic[0] # save a dictionary model
+                temp.append(strs[0])
+                del strs[0]
+                del list_dic[0]
+
+                while j < len(list_dic):
+                    if list_dic[j] == compared_dic:
+                        temp.append(strs[j])
+                        del strs[j]
+                        del list_dic[j]
+                        continue
+                    j += 1
+            if temp:
+                rev.append(temp)
+        return rev  
+            
---
+++ Group Anagrams/readme.md
@@ -0,0 +1,23 @@
+Given an array of strings, group anagrams together.
+
+## Example:
+
+* Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+* Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+## Note:
+
+* All inputs will be in lowercase.
+* The order of your output does not matter.
+
+## 我的心得:
+* 寫這題時的我的順序一直想錯(汗)
+* 我的作法是使用dictionary比對，只要dictionary相同就相同，並要加入同一個list的list
+* 每次都是用list[0]比對其他的，比對前要先拔掉list[0]，然後再比對剩下的
+* 在判斷dictionary相同與否時，若相同，就可以拔掉，但要小心該index要保留為原來的(所以要用continue跳過)，因為[1,3,4,5]拔掉3後list會成為[1,4,5]，原來拔掉的3 index是1，後來的list的index為1的部分剛好是4
+* 如此做法並不是最好的，因為如果遇到["abc","cdv","dwr",....](沒有一個是其中的permutation)，此即為 worst case
+* 所以時間複雜度為O($n^2$)
