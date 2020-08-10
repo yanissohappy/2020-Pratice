6eff4ba0ab3dee8493cce7f833d96313ba8fed11
Yan <41467560+yanissohappy@users.noreply.github.com>
2020-08-10 16:36:15 +0800

    Palindrome Partitioning :poop:
    Still need to think twice

---
+++ Palindrome Partitioning/main.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def DFS(self, s, a_list, ret):
+        if not s:
+            ret.append(a_list)
+            # print(ret)
+            return
+        
+        i = 1
+        while i < len(s) + 1:
+            if s[:i][::-1] == s[:i]: # what we get now is palindrome
+                #a_list.append(s[:i])
+                self.DFS(s[i:], a_list + [s[:i]], ret)          
+                #a_list.pop()
+            i += 1
+            
+    def partition(self, s):
+        ret = []
+        self.DFS(s, [], ret)
+        return ret
+
+            
+            
---
+++ Palindrome Partitioning/readme.md
@@ -0,0 +1,19 @@
+Given a string s, partition s such that every substring of the partition is a palindrome.
+
+* Return all possible palindrome partitioning of s.
+
+## Example:
+
+* Input: "aab"
+* Output:
+[  
+  ["aa","b"],  
+  ["a","a","b"]  
+]  
+
+## [原題目連結點我](https://leetcode.com/problems/palindrome-partitioning/)
+	
+## 我的心得:
+* 思路如下：
+
+![avatar](./image0.jpeg)
