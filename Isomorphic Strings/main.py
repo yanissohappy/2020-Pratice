82573b326b9eeb39eb980b1f45492e03f14d35ad
Yan <41467560+yanissohappy@users.noreply.github.com>
2020-08-17 04:36:30 +0800

    Update main.py

--- Isomorphic Strings/main.py
+++ Isomorphic Strings/main.py
@@ -2,7 +2,8 @@ class Solution(object):
     def isIsomorphic(self, s, t):
         tuples = list(zip(s, t))
         set_tuples = set(tuples)
-        set_s = []
-        for item, _ in set_tuples:
-            set_s.append(item)
-        return len(set_s) == len(set(s))
+        set_s, set_t = [], []
+        for item_s, item_t in set_tuples:
+            set_s.append(item_s)
+            set_t.append(item_t)
+        return len(set_s) == len(set(s)) and len(set_t) == len(set(t))
