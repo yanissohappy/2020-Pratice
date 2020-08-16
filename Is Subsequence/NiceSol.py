9ba3565c09c68d588d087556b6ed35fa02c89d02
Yan <41467560+yanissohappy@users.noreply.github.com>
2020-08-17 07:01:35 +0800

    Update NiceSol.py

--- Is Subsequence/NiceSol.py
+++ Is Subsequence/NiceSol.py
@@ -1,11 +1,12 @@
 from collections import defaultdict
+import bisect as bi
 class Solution(object):
 	def isSubsequence(self, s, t):
 		idx = defaultdict(list)
 		for i, c in enumerate(t):
 			idx[c].append(i)
 		prev = 0
-		for i, c in enumerate(s):            
+		for c in s:            
 			j = bi.bisect_left(idx[c], prev)
 			if j == len(idx[c]): return False
 			prev = idx[c][j] + 1
